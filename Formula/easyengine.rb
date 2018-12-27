5cc997c9f5494c1da85c21928ac13a1bf999e104
Abhijit Rakas <abhijit.rakas@rtcamp.com>
2018-12-27 21:39:29 +0530

    easyengine 4.0.21

--- Formula/easyengine.rb
+++ Formula/easyengine.rb
@@ -1,8 +1,9 @@
 class Easyengine < Formula
   desc "Command-line control panel to manage WordPress sites"
   homepage "https://easyengine.io/"
-  url "https://github.com/EasyEngine/easyengine/releases/download/v4.0.8/easyengine.phar"
-  sha256 "020d0b48329c1864d06e6a19ae48b4c1416c92645bcd9b13990565ad52d6f163"
+  url "https://github.com/abhijitrakas/easyengine/releases/download/v4.0.21/easyengine.phar"
+  sha256 "7831cdb765afa04a9517b92a1eb00a6e26459aad33ddbb12ed786720564231af"
+  revision 1
 
   bottle :unneeded
 
